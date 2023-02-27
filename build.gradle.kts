e5402f14af8a06c0044d09191d37e71cafdb28f3
pierre <pierremarais67@gmail.com>
2023-02-27 15:00:33 +0100

    Implement basic producer and consumer

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.apache.kafka:kafka-clients:3.0.0")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,61 @@
+import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.clients.consumer.ConsumerRecords
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerRecord
+import java.time.Duration
+import java.time.LocalDateTime
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
 fun main(args: Array<String>) {
-    println("Hello World!")
+    fun createProducer(): Producer<String, String> {
+        val props = Properties()
+        props["bootstrap.servers"] = "localhost:9092"
+        props["acks"] = "all"
+        props["retries"] = 0
+        props["linger.ms"] = 1
+        props["key.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
+        props["value.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
+        return KafkaProducer(props)
+    }
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    fun Producer<String, String>.produceMessages(topic: String) {
+        fixedRateTimer(daemon = true, period = Duration.ofSeconds(2).toMillis()) {
+            val time = LocalDateTime.now()
+            val message = ProducerRecord(
+                topic, // topic
+                time.toString(), // key
+                "Message sent at ${LocalDateTime.now()}" // value
+            )
+            send(message)
+        }
+    }
+
+    fun createConsumer(): Consumer<String, String> {
+        val props = Properties()
+        props.setProperty("bootstrap.servers", "localhost:9092")
+        props.setProperty("group.id", "test")
+        props.setProperty("enable.auto.commit", "true")
+        props.setProperty("auto.commit.interval.ms", "1000")
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+        return KafkaConsumer(props)
+    }
+
+    fun Consumer<String, String>.consumeMessages(topic: String) {
+        subscribe(listOf(topic))
+        while (true) {
+            val messages: ConsumerRecords<String, String> = poll(Duration.ofMillis(5000))
+            for (message in messages) {
+                println("Consumer reading message: ${message}")
+            }
+            commitAsync()
+        }
+    }
+
+    val producer = createProducer()
+    producer.produceMessages("topic1")
+    val consumer = createConsumer()
+    consumer.consumeMessages("topic1")
 }
