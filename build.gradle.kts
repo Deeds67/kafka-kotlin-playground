30cb8aefd0fe6f8821e7f75b2bbd395a24c5b084
rvdw <rohan.vanderwalt@gmail.com>
2024-03-19 18:57:47 +0100

    failing forward

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     implementation("org.apache.kafka:kafka-clients:3.6.1")
     implementation("org.apache.flink:flink-java:${flinkVersion}")
 
+    implementation("org.apache.flink:flink-clients:${flinkVersion}")
     implementation("org.apache.flink:flink-connector-base:${flinkVersion}")
     implementation("org.apache.flink:flink-connector-kafka:3.1.0-1.18")
     implementation("org.apache.flink:flink-table-api-java-bridge:${flinkVersion}")
